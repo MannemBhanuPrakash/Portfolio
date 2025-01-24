9d87c9847a76a5a327605bf53aeed53e4f091f06
Bhanu Prakash Mannem <90899788+MannemBhanuPrakash@users.noreply.github.com>
2025-01-24 11:05:16 +0530

    build

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 
-// import Test from './pages/test';
-// import Router from './router/Routes';
 
 
 function App() {
   return (
     <>
-      {/* <Test/> */}
       <Header/>
       <Home/>
       <About/>
       <Contact />
-      {/* <Header />
-      <Router/> */}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import { ThemeProvider } from "./ThemeContext";
 import { AlertProvider } from './common/Alert';
-// import HotjarInit from './hotjar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
       <AlertProvider>
-        <HotjarInit/>
         <App />
       </AlertProvider>
     </ThemeProvider>
